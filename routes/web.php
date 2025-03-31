be201b7a29cfabc5c54c6c561fc78a7113bbc231
stemastr <stevenadi16@gmail.com>
2025-03-31 10:14:03 +0700

    add routes try blade template

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,42 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
 });
 
 
 Route::get('/login', function () {
     return view('auth/login');
+});
+
+
+Route::get('/register', function () {
+    return view('auth/register');
+});
+
+
+Route::get('/pasien', function () {
+    return view('pasien/dashboard');
+});
+
+
+Route::get('/pasien/periksa', function () {
+    return view('pasien/periksa');
+});
+
+
+Route::get('/pasien/riwayat', function () {
+    return view('auth/riwayat');
+});
+
+Route::get('/dokter', function () {
+    return view('dokter/dashboard');
+});
+
+Route::get('/dokter/periksa', function () {
+    return view('dokter/periksa');
+});
+
+Route::get('/dokter/obat', function () {
+    return view('dokter/obat');
 });
